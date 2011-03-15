926c37b7c1a77c38023d6e81f39a03b1a34d43fe
Kevin Charter <kevin.charter@acm.org>
2011-03-14 23:13:12 -0600

    Added my solution to problem 3.

---
+++ problem3.hs
@@ -0,0 +1,35 @@
+module Main where
+
+-- Problem 3:
+
+-- The prime factors of 13195 are 5, 7, 13 and 29.
+
+-- What is the largest prime factor of the number 600851475143 ?
+
+-- Solution
+
+-- First, we'll use the sieve of Eratosthenes to generate the list of primes.
+
+seive :: [Integer]
+seive = seive' [2..]
+  where seive' (n:rest) =
+          n:seive' (filter (not . multiple n) rest)
+        multiple n m = m `mod` n == 0
+        
+-- To find the maximum prime divisor, we divide by larger and larger
+-- prime divisors, until we encounter a prime that is larger than the
+-- result of the division. Note that the seive is already sorted in
+-- ascending order.
+        
+largestPrimeDivisor n = largestDivisor n 1 (head seive) (tail seive)
+
+
+largestDivisor n last candidate cs@(next:rest) =
+  if candidate > n
+  then last
+  else if n `mod` candidate == 0
+       then largestDivisor (n `div` candidate) candidate candidate cs
+       else largestDivisor n last next rest
+  
+main :: IO ()
+main = print $ largestPrimeDivisor 600851475143
